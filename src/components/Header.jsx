b5733f4c27e3114f6f511d2647669946edb9cef6
theBwoyProgrammer <hatimeddinani@gmail.com>
2024-01-25 16:52:19 +0300

    fixed layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Pearl Bets</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/packages">Packages</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
